a408d1f8aa19eec02959e781499508064fb32348
Chris Horton <hortoncd@gmail.com>
2014-12-18 19:26:03 -0800

    make test kitchen work with serverspec ver > 2

--- test/integration/default/serverspec/lita_spec.rb
+++ test/integration/default/serverspec/lita_spec.rb
@@ -1,11 +1,10 @@
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+set :backend, :exec
 
 RSpec.configure do |c|
   c.before :all do
-    c.path = '/sbin:/usr/sbin'
+    c.path = '/bin:/usr/bin:/sbin:/usr/sbin'
   end
 end
 
